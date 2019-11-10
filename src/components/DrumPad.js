c576c7b0f1d3beef4f052bda5114e943cdf26456
Relsig <Chris@honoredpath.com>
2019-11-10 13:10:10 -0600

    implement drum-pad

---
+++ src/components/DrumPad.js
@@ -0,0 +1,50 @@
+import React from './react';
+
+class DrumPad extends React.Component {
+    componentDidMount() {
+        console.log(this.audio);
+        document.addEventListener('keydown', this.handleKeyDown);
+        window.focus();
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handleKeyDown);
+    }
+
+    playAudio = () => {
+        this.audio.play();
+        this.audio.currentTime = 0;
+        this.props.handleDisplay(this.props.id);
+
+    }
+
+    handleKeydown = key => {
+        if(key.keyCode === this.props.letter.charCodeAt()){
+            this.playAudio();
+        }
+    }
+
+    handleClick = () => {
+        this.playAudio();
+    }
+
+    render() {
+        return (
+            <div
+                classname='drum-pad'
+                id={this.props.id}
+                onClick={this.handleClick}
+            >
+                <h1>{this.props.letter}</h1>
+                
+                <audio id={this.props.letter}
+                        className='clip'
+                        src={this.props.src}
+                        ref={ref => this.audio = ref}
+                />
+            </div>
+        );
+    }
+}
+
+export default DrumPad;
